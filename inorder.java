bb4b48a592ba594ce9000ba9eb8e0e3420b30aba
ghost-kali <tamimofficial122005@gmail.com>
2023-11-27 21:58:08 +0530

    inorder Traversal

---
+++ inorder.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class inorder
+{
+    List<Integer> a=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root!=null){
+            preorderTraversal(root.left);
+            a.add(root.val);
+            preorderTraversal(root.right);
+        }
+
+        return a;
+    }
+}
+
+/*Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ */
